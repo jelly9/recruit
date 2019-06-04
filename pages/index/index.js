90d352bd523cd16019a48e8dd7dc726d2b5137f0
jelly9 <xgd9543@gmail.com>
2019-06-04 20:46:25 +0800

    up

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,7 +6,11 @@ const app = getApp()
 
 Page({
   data: {
-    postList: null
+    postList: null,
+    // 输入框内容
+    keyword: '',
+    inputShowed: false
+
   },
   onShow: function() {
     var that = this
@@ -22,5 +26,35 @@ Page({
         console.log("fail: ", res)
       }
     })
-  }
+  },
+  // 输入框发生变化时触发
+  inputTyping: function (e) {
+    this.setData({
+      keyword: e.detail.value,
+      inputShowed: true
+    });
+  },
+  // 当有输入时触发
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
+  },
+  // 当点击取消时触发
+  hideInput: function () {
+    this.setData({
+      inputShowed: false,
+      keyword: '',
+      shopList: [],
+      isDisplayCreateNew: false
+    });
+  },
+
+  // 当点击清楚按钮 (X) 时触发
+  clearInput: function () {
+    this.setData({
+      keyword: '',
+      inputShowed: false
+    });
+  },
 })
